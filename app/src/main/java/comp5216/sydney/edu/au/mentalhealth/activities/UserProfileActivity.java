744239b63d595bff1dab19b0e59ea793990bc4c5
Haibo Ye <haye0091@uni.sydney.edu.au>
2023-10-03 17:59:04 +1100

    init

---
+++ app/src/main/java/comp5216/sydney/edu/au/mentalhealth/activities/UserProfileActivity.java
@@ -0,0 +1,16 @@
+package comp5216.sydney.edu.au.mentalhealth.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import comp5216.sydney.edu.au.mentalhealth.R;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
